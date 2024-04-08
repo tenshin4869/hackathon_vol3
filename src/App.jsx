24c1a288337119ee2716c89be5c2a103cb03cae7
ichisemarika <159396316+ichisemarika@users.noreply.github.com>
2024-04-08 22:35:08 +0900

    ログイン悪戦苦闘中

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SignUp from "./components/SignUp";
+
+function App() {
+  return (
+    <div style={{ margin: "2em" }}>
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignUp.js
@@ -0,0 +1,39 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase.js";
+import { useNavigate } from "react-router-dom";
+
+function SignUp() {
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((auth) => {
+        if (auth) {
+          navigate("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <div>
+      <h1>ユーザ登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input name="email" type="email" placeholder="email" />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
