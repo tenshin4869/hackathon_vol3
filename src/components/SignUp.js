d1ed9053881620eca095012c66651909fce6ea9c
ichisemarika <159396316+ichisemarika@users.noreply.github.com>
2024-04-09 18:46:47 +0900

    たぶんSignUp修復済み

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,18 +2,59 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase.js";
 import { useNavigate } from "react-router-dom";
 
-function SignUp() {
-  const navigate = useNavigate();
+// function SignUp() {
+//   const navigate = useNavigate();
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const { email, password } = event.target.elements;
+//     createUserWithEmailAndPassword(auth, email, password)
+//       .then((auth) => {
+//         if (auth) {
+//           navigate("/home");
+//         }
+//       })
+//       .catch((error) => alert(error.message));
+//   };
+
+//   return (
+//     <div>
+//       <h1>ユーザ登録</h1>
+//       <div onclick={handleSubmit}>
+//         <div>
+//           <label>メールアドレス</label>
+//           <input name="email" type="email" placeholder="email" />
+//         </div>
+//         <div>
+//           <label>パスワード</label>
+//           <input name="password" type="password" />
+//         </div>
+//         <div>
+//           <button type="button">登録</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SignUp;
+
+const SignUp = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const { email, password } = event.target.elements;
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((auth) => {
-        if (auth) {
-          navigate("/home");
-        }
+    console.log(email.value, password.value);
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+      .then((userCredential) => {
+        // ユーザーのサインアップが成功した場合の処理
+        const user = userCredential.user;
+        console.log("User signed up:", user);
       })
-      .catch((error) => alert(error.message));
+      .catch((error) => {
+        // サインアップに失敗した場合の処理
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Sign up error:", errorCode, errorMessage);
+      });
   };
 
   return (
@@ -34,6 +75,6 @@ function SignUp() {
       </form>
     </div>
   );
-}
+};
 
 export default SignUp;
