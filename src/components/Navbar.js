b9279eb193c729c1c33b1e71d88819e2005c6a74
tenshin4869 <ten20031017-sty@ezweb.ne.jp>
2024-04-11 21:25:41 +0900

    createPost追加

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,66 @@
 import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+import DrawerComponent from "./Drawer";
 
-export default function Navbar() {
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(5),
+    display: "flex",
+  },
+  logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/createpost">Post</Link>
-      <Link to="login">Login</Link>
-    </nav>
+    <AppBar position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" className={classes.logo}>
+          Navbar
+        </Typography>
+        {isMobile ? (
+          <DrawerComponent />
+        ) : (
+          <div className={classes.navlinks}>
+            <Link to="/" className={classes.link}>
+              Home
+            </Link>
+            <Link to="/createpost" className={classes.link}>
+              Post
+            </Link>
+            <Link to="/login" className={classes.link}>
+              Login
+            </Link>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 }
+export default Navbar;
